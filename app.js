55a3d6db21d515cc05c5e7f4cc097dc773c16088
Mikhail Biltsevich <muxacb.brest@gmail.com>
2017-03-09 14:21:40 +0300

    Добавлена авторизация, выход из профиля и сессии

--- app.js
+++ app.js
@@ -2,21 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./dbConfig');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
 const port = 8080;
 
 app.use(bodyParser.json());
+app.use(session({
+  secret: 'this is the black cat of luck', 
+  saveUninitialized: false,
+  resave: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 mongoose.connect(db.url, function (err) {
-    if (err) {
-      return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 
-    require('./routes')(app);
+  require('./routes')(app);
 
-    app.listen(port, function () {
-        console.log('We are live on ' + port);
-      });
-  });
+  app.listen(port, function () {
+      console.log('We are live on ' + port);
+    });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,11 +10,40 @@ module.exports = function (app) {
 
     User.create({login: login, dataPassword: {password, repeatedPassword}})
       .then(function (doc) {
+        req.session.user = login;
         response.redirect('/');
       })
       .catch(function (err) {
         console.log(err.message);
         response.send('При регистрации произошла ошибка. Попробуйте снова');
       });
+  });
+
+  app.post('/login', function (request, response) {
+    var login = request.body.login;
+    var password = request.body.password;
+
+    User.findOne({login: login})
+      .then(function (doc) {
+        doc.comparePasswords(password)
+          .then(function (isSuccess) {
+            if (isSuccess) {
+              request.session.user = doc.login;
+            }
+            response.redirect('/');
+          });
+      })
+      .catch(function (err) {
+        response.redirect('/');
+      });
+  });
+
+  app.get('/logout', function (request, response) {
+    request.session.destroy(function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    response.redirect('/');
   });
 };
