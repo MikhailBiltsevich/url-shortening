9cc6df66de5258bec6a99f1cb2eaf55e78e96f36
Mikhail Biltsevich <muxacb.brest@gmail.com>
2017-03-14 17:49:16 +0300

    Изменен хост для бд, чтобы работало локально без интернета

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.db = {};
 config.port = 8080;
 config.webhost = 'http://localhost:' + config.port + '/';
 
-config.db.host = 'localhost';
+config.db.host = '127.0.0.1';//localhost
 config.db.port = 27017;
 config.db.name = 'url-shortening';
 
